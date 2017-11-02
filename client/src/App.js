1a3b83579e1ac929e18fe8be7d5399548a7c087d
Peter Margaritoff <peter@resy.com>
2017-11-02 15:09:27 -0400

    Adds bootstrap and sets up some base components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -33,7 +32,7 @@ class App extends Component {
 
   authenticate = () => {
     const {passwordInput, usernameInput} = this.state;
-    
+
     const headers = new Headers({
       'Content-Type': 'application/json'
     });
@@ -61,14 +60,51 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!!</h1>
-        </header>
-        <input onChange={this.handleUsernameInput} type="text" />
-        <input onChange={this.handlePasswordInput} type="password" />
-        <button onClick={this.authenticate}>Authenticate</button>
+      <div>
+        <nav className="navbar navbar-dark bg-dark">
+          <span className="navbar-brand mb-0 h1">Navbar</span>
+        </nav>
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-2">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">Active</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col">
+              <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                  <h1 className="display-3">Fluid jumbotron</h1>
+                  <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </div>
+              </div>
+              <div className="card" style={{width: '20rem'}}>
+                <img className="card-img-top" src="http://placehold.it/200x200" alt="Card image cap" />
+                <div className="card-body">
+                  <h4 className="card-title">Card title</h4>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+
+              <input onChange={this.handleUsernameInput} type="text" />
+              <input onChange={this.handlePasswordInput} type="password" />
+              <button onClick={this.authenticate}>Authenticate</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
